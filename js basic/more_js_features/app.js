9d2410427fa7acbc6b6e95749de44d36db4cc202
Alex Wu <alexwu727@gmail.com>
2021-08-02 16:41:36 +0800

    spread and rest params

--- js basic/more_js_features/app.js
+++ js basic/more_js_features/app.js
@@ -7,8 +7,28 @@ const rollDie = (numSides = 6) => Math.floor(Math.random() * numSides) + 1;
 function greet(person, msg = "Hey there") {
     console.log(`${msg}, ${person}!`)
 }
+
+// spread
 const nums = [3, 4, 5, 6]
 let a = Math.max(nums) // NaN
 let b = Math.max(...nums) // 6
 console.log(nums)
 console.log(...nums)
+
+const cats = ["Blue", "Scout", "Rocket"]
+const dogs = ["Rusty", "Wyatt"]
+const allPets = [...cats, ...dogs, "Speedy"]
+
+// spread with object
+const feline = { legs: 4, family: "Felidae" }
+const canine = { isFurry: true, family: "Caninae" }
+const newObject = { ...feline, ...canine }
+
+// rest params
+const sum = (...nums) => nums.reduce((total, num) => total + num)
+
+function raceResults(gold, silver, ...everyoneElse) {
+    console.log(`Gold medal goes to ${gold}`)
+    console.log(`Silver medal goes to ${silver}`)
+    console.log(`and thanks to everyone else: ${everyoneElse}`)
+}
