4e28c3655dede5e143edb163430dbe06d4249514
Alex Wu <alexwu727@gmail.com>
2021-08-03 14:56:16 +0800

    manipulating

---
+++ DOM/Manipulating/app.js
@@ -0,0 +1,12 @@
+const allLinks = document.querySelectorAll('a');
+
+for (let link of allLinks) {
+    link.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+}
+
+
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+// }
