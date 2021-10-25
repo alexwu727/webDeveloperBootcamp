0c479a1b69d4dcc394eaa432635d40cc3a43ea50
Alex Wu <alexwu727@gmail.com>
2021-10-24 22:34:29 -0700

    mongoose basic 1

---
+++ js/node/MongooseBasics/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/movieApp", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("connection open!");
+    })
+    .catch(err => {
+        console.log("Oh no!");
+        console.log(err);
+    })
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model("Movie", movieSchema)
+const amadeus = new Movie({ title: "Amadeus", year: 1986, score: 9.2, rating: "R" })
+amadeus.save()
+
+
